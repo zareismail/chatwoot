e4704954f5c30824035b8fee4b32969eff56293c
Ismail Zare <zarehesmaiel@gmail.com>
2026-07-02 08:30:52 +0330

    force to show messages when open conversation

--- app/javascript/widget/App.vue
+++ app/javascript/widget/App.vue
@@ -319,16 +319,18 @@ export default {
         } else if (message.event === 'toggle-open') {
           this.$store.dispatch('appConfig/toggleWidgetOpen', message.isOpen);
 
-          const shouldShowMessageView =
-            ['home'].includes(this.$route.name) &&
-            message.isOpen &&
-            this.messageCount;
+          const shouldEnterChatView =
+            ['home'].includes(this.$route.name) && message.isOpen;
           const shouldShowHomeView =
             !message.isOpen &&
             ['unread-messages', 'campaigns'].includes(this.$route.name);
 
-          if (shouldShowMessageView) {
-            this.router.replace({ name: 'messages' });
+          if (shouldEnterChatView) {
+            if (this.preChatFormEnabled && !this.conversationSize) {
+              this.router.replace({ name: 'prechat-form' });
+            } else {
+              this.router.replace({ name: 'messages' });
+            }
           }
           if (shouldShowHomeView) {
             this.$store.dispatch('conversation/setUserLastSeen');
